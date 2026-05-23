475983d2ace953742e3316cbfaab2dbed1e2343e
tehPsycho <houtchenss@gmail.com>
2026-05-23 14:46:46 -0400

    Remove TypeScript/Next.js files and keep static HTML/CSS/JS setup

--- src/app/products/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ProductsExplorer } from './products-explorer';
-import { productRepository } from '@/lib/data/products';
-
-export default function ProductsPage() {
-  const products = productRepository.getAll();
-  return <ProductsExplorer products={products} />;
-}
